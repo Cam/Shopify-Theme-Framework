e1ce801f6c17d38fcfa8390b57f2991d94ceac70
Cam <social@camgould.com>
2013-09-02 10:55:05 +1000

    'Cleaned Up and Shipped Out'
    This is the final beta version of this theme. It is now pretty solid,
    but will have some more additions before 1.0

--- assets/jquery.gallery.js
+++
@@ -1,8 +0,0 @@
-/*!
-	Basic Image Gallery jQuery thanks to Nimpkish http://www.nimpkish.com/blog/simple-javascript-image-gallery
-*/
-$('#images_thumbnails a').click(function(){
-  var newImageSrc = $(this).attr('href');
-  $('#images_full img').attr({'src': newImageSrc });
-  return false;
-});
